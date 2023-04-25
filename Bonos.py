ee9f126231cfe3a890f17ee3a76531a46639bd5f
BasilioSalazarN <116610544+BasilioSalazarN@users.noreply.github.com>
2023-04-25 10:21:45 -0600

    Update Bonos.py

--- Bonos.py
+++ Bonos.py
@@ -117,21 +117,21 @@ def Bonos(usuario,puesto):
         bonos_variables_9=0
         bonos_fijos_9=0
         otros_bonos_9=0
-        
-
+       
         for a in range(1,pivot1):
 
             bonos_variables_9 = bonos_variables_9 + sum([float(bonos_9.iloc[a,65]),float(bonos_9.iloc[a,66]),float(bonos_9.iloc[a,67]),float(bonos_9.iloc[a,68]),float(bonos_9.iloc[a,69]),float(bonos_9.iloc[a,70]),float(bonos_9.iloc[a,71]),float(bonos_9.iloc[a,72]),float(bonos_9.iloc[a,73])]                                            )
             bonos_fijos_9 = bonos_fijos_9 + sum([float(bonos_9.iloc[a,75]),float(bonos_9.iloc[a,76]),float(bonos_9.iloc[a,77]),float(bonos_9.iloc[a,78]),float(bonos_9.iloc[a,79]),float(bonos_9.iloc[a,80]),float(bonos_9.iloc[a,81]),float(bonos_9.iloc[a,82]),float(bonos_9.iloc[a,83])])
             otros_bonos_9 = otros_bonos_9 + sum([float(bonos_9.iloc[a,95]),float(bonos_9.iloc[a,96]),float(bonos_9.iloc[a,97]),float(bonos_9.iloc[a,98])])
-
+            bonos_total=sum([float(bonos_variables_9),float(bonos_fijos_9),float(otros_bonos_9)])
+            
         placeholdercols = st.empty()
         col1, col2, col3, col4 = placeholdercols.columns(4)
         col1.metric("Bonos Variables",bonos_variables_9)
         col2.metric("Bonos Fijos",bonos_fijos_9)
         col3.metric("Otros Bonos",otros_bonos_9)
-        col4.metric("Total",bonos_9.iloc[0,101])
-
+        col4.metric("Total",bonos_total)
+        
     else:
     
       bonos_9= pd.read_sql(f"select a0,a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,a17,a18,a19,a20,a21,a22,a23,a24,a25,a26,a27,a28,a29,a30,a31,a32,a33,a34,a35,a36,a37,a38,a39,a40,a41,a42,a43,a44,a45,a46,a47,a48,a49,a50,a51,a52,a53,a54,a55,a56,a57,a58,a59,a60,a61,a62,a63,a64,a65,a66,a67,a68,a69,a70,a71,a72,a73,a74,a75,a76,a77,a78,a79,a80,a81,a82,a83,a84,a85,a86,a87,a88,a89,a90,a91,a92,a93,a94,a95,a96,a97,a98,a99,a100,a101,a102 from bonos where a1='{personal_9}' and a102='{periodo_bonos_9}'", con)
