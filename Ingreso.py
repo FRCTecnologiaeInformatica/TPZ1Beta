5797e6c64b29ee6c7946c6390e112af06e75a4b3
BasilioSalazarN <116610544+BasilioSalazarN@users.noreply.github.com>
2023-05-17 21:50:15 -0600

    Update Ingreso.py

--- Ingreso.py
+++ Ingreso.py
@@ -16,7 +16,7 @@ hide_menu_style = """
         #MainMenu {visibility: hidden;}
         footer {visibility:visible;}
         footer:after{
-            content:'V.1.7 Copyrigth @ 2023 Telespazio Argentina S.A.';
+            content:'V.1.8 Copyrigth @ 2023 Telespazio Argentina S.A.';
             display: block;
             position: relative;
             color: tomato;
