98187f3379c25feff06a941a08e148c66b3e11e6
BasilioSalazarN <116610544+BasilioSalazarN@users.noreply.github.com>
2023-05-17 21:15:23 -0600

    Update Historial.py

--- Historial.py
+++ Historial.py
@@ -302,7 +302,7 @@ def Historial(usuario,puesto):
     placeholder17_7 = st.empty()
     horas_7=placeholder17_7.subheader("Resumen de Horas")  
 
-    data_2_r = data_1_r.groupby(["nombre", "fecha"], as_index=False)["predios","horas"].agg(np.sum)
+    data_2_r = data_1_r.groupby(["nombre", "fecha"], as_index=False)[["predios","horas"]].agg(np.sum)
   
     pivot_r=len(data_2_r.iloc[:,0])
     pivot_c=len(data_2_c.iloc[:,0])
@@ -319,8 +319,8 @@ def Historial(usuario,puesto):
       nombre_c=data_2_c.iloc[:,0]
       nombre_o=data_2_o.iloc[:,0]
 
-      nombre_horas= nombre_r.append(nombre_c)
-      nombre_horas= nombre_horas.append(nombre_o)
+      nombre_horas= nombre_r.concat([nombre_c])
+      nombre_horas= nombre_horas.concat([nombre_o])
 
       fecha_r=data_2_r.iloc[:,1]
       fecha_c=data_2_c.iloc[:,1]
