a7fd0acbc77b59d1e32f780b3333f1dde970fae6
BasilioSalazarN <116610544+BasilioSalazarN@users.noreply.github.com>
2025-01-07 14:48:18 -0600

    Update Ingreso.py

--- Ingreso.py
+++ Ingreso.py
@@ -16,7 +16,7 @@ hide_menu_style = """
         #MainMenu {visibility: hidden;}
         footer {visibility:visible;}
         footer:after{
-            content:'V.1.22 Copyrigth @ 2023 Telespazio Argentina S.A.';
+            content:'V.1.23 Copyrigth @ 2023 Telespazio Argentina S.A.';
             display: block;
             position: relative;
             color: tomato;
@@ -156,4 +156,4 @@ if pivot!=1:
 
      st.subheader("En caso de dudas favor revisar el siguiente video tutorial en el apartado 16. Registros y Bonos de la wiki")
 
-     st.subheader("Para soporte técnico favor escribir a basilio.nunez@telespazio.com")
+     st.subheader("Para soporte técnico favor escribir a daniela.rivera@telespazio.pe")
