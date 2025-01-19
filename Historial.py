8f7a81dbe3e66bcc30abc4f57259ce9a17c609ac
BasilioSalazarN <116610544+BasilioSalazarN@users.noreply.github.com>
2025-01-19 14:38:30 -0600

    Update Historial.py

--- Historial.py
+++ Historial.py
@@ -331,15 +331,13 @@ def Historial(usuario,puesto):
 
       datos_horas = pd.DataFrame(data={"nombre":nombre_horas,"fecha":fecha_horas}).groupby(["nombre","fecha"],as_index=False).size()
 
-     # datos_horas["Horas_Producción"] = 0.0000
-     # datos_horas["Horas_Capacitación"] = 0.0000
-      # datos_horas["Horas_Otros_Registros"] = 0.0000
-
       datos_horas = pd.merge(datos_horas,data_2_r, on=['nombre','fecha'], how="left") 
       datos_horas = pd.merge(datos_horas,data_2_c, on=['nombre','fecha'], how="left") 
       datos_horas = pd.merge(datos_horas,data_2_o, on=['nombre','fecha'], how="left") 
 
-    # datos_horas["Total"]= datos_horas.iloc[:,3:6].sum(axis=1)
+      datos_horas= datos_horas.fillna(0)
+
+      datos_horas["Total"]= datos_horas.iloc[:,3:6].sum(axis=1)
 
       placeholder19_7 = st.empty()
       historial_7_horas= placeholder19_7.dataframe(data=datos_horas)
